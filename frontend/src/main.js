9413607fe2fcb0eae1295ff1f122eec3cb8482bc
enrico-kaack <e.kaack@live.de>
2019-10-25 20:53:54 +0200

    Setup: Frontend with main components

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
-import VueRouter from 'vue-router'
+import VueRouter from "vue-router";
 import App from "./App.vue";
-
+import vuetify from "./plugins/vuetify";
 
 Vue.config.productionTip = false;
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
