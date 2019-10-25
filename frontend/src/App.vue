3c04eab4aa6db286f62a05f359a041af8d5e76cf
enrico-kaack <e.kaack@live.de>
2019-10-25 21:58:15 +0200

    Fixed tab bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <v-app id="app">
-    <MainComponent msg="Welcome to Your Vue.js App" />
-    <v-footer color="primary" padless>
-      <v-row align="center" justify="center" no-gutters>
-        <v-col><v-btn>Main</v-btn></v-col>
-        <v-col><v-btn>Action</v-btn></v-col>
-        <v-col ><v-btn>Quiz</v-btn></v-col>
-      </v-row>
-    </v-footer>
+    <v-content>
+      <MainComponent msg="Welcome to Your Vue.js App" />
+    </v-content>
+    <v-bottom-navigation>
+      <v-btn>
+        <span>Main</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn>
+        <span>Action</span>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+      <v-btn>
+        <span>Quiz</span>
+        <v-icon>mdi-help</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
