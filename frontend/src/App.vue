26985ca7986703e7461aafd45ac4db8a4e446201
Lukas Klassen <luftigo@yahoo.de>
2019-10-25 23:03:56 +0200

    feat: Implemented Routing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,15 +4,15 @@
       <MainComponent msg="Welcome to Your Vue.js App" />
     </v-content>
     <v-bottom-navigation>
-      <v-btn>
+      <v-btn v-on:click="navMain">
         <span>Main</span>
         <v-icon>mdi-home</v-icon>
       </v-btn>
-      <v-btn>
+      <v-btn v-on:click="navAction">
         <span>Action</span>
         <v-icon>mdi-home</v-icon>
       </v-btn>
-      <v-btn>
+      <v-btn v-on:click="navQuiz">
         <span>Quiz</span>
         <v-icon>mdi-help</v-icon>
       </v-btn>
@@ -27,6 +27,17 @@ export default {
   name: "app",
   components: {
     MainComponent
+  },
+  methods: {
+    navMain: function() {
+      this.$router.push({ path: "/" });
+    },
+    navAction: function() {
+      this.$router.push({ path: "/action" });
+    },
+    navQuiz: function() {
+      this.$router.push({ path: "/quiz" });
+    }
   }
 };
 </script>
