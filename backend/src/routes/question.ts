60beb5e1ae909fa87908763eb7b42cc878c26213
Lukas Klassen <luftigo@yahoo.de>
2019-10-25 21:36:55 +0200

    feat: Added structure for Question MiddleWare

---
+++ backend/src/routes/question.ts
@@ -0,0 +1,8 @@
+import Router from 'koa-router';
+import Koa from 'koa';
+
+function QuestionMW (ctx : Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>){
+
+}
+
+export default QuestionMW;
