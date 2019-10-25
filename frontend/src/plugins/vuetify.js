05f1f8eae2d73de5f25278b83262ca337bd49713
enrico-kaack <e.kaack@live.de>
2019-10-26 00:09:37 +0200

    Added: custom theme

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -6,5 +6,18 @@ Vue.use(Vuetify);
 export default new Vuetify({
   icons: {
     iconfont: "mdi"
-  }
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: '#2e3440',
+        secondary: '#4c566a',
+        accent: '#50fa7b',
+        error: '#b71c1c',
+      },
+      options: {
+        customProperties: true,
+      },
+    },
+  },
 });
