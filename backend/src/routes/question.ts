4aad91efac2581e450491bf24a38b03ade184c3c
Lukas Klassen <luftigo@yahoo.de>
2019-10-25 21:46:48 +0200

    feat: Implemented GetQuestionOfDay

--- backend/src/routes/question.ts
+++ backend/src/routes/question.ts
@@ -1,8 +1,26 @@
 import Router from 'koa-router';
 import Koa from 'koa';
 
-function QuestionMW (ctx : Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>){
-
+interface Question {
+	question: string,
+	answers: string[],
+	correctAnswerIndex: number,
+	answerExplanation: string,
+	source: string
 }
+
+const questions : Question[] = require("../quiz/questions.json");
+let day = 0;
+
+/**
+ * Call for /getQuestionOfDay
+ * @param ctx Router Context
+*/
+function QuestionMW (ctx : Koa.ParameterizedContext<any, Router.IRouterParamContext<any, {}>>){
+	ctx.body = questions[day % questions.length];
+}
+
+//every day increment day counter
+setInterval(() => day ++,86400000);
 
 export default QuestionMW;
