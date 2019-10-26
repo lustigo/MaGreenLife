414999e26b4b065ba09e5de195653ac0ad0ade78
Lukas Klassen <luftigo@yahoo.de>
2019-10-26 12:25:30 +0200

    feat: Added PointsPost and Route Documentation

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,5 +1,6 @@
 import Router from 'koa-router';
 import rnvGetStationMonitorMW from './getStationMonitor';
+import {PointsGet, PointsPost} from './points';
 import QuestionMW from './question';
 
 const router = new Router();
@@ -8,8 +9,24 @@ router.get('/', (ctx) => {
   ctx.body = 'Hallo Welt';
 });
 
+/**
+ * Parameter: Uid
+ */
+router.get('/points', PointsGet);
+
+/**
+ * Body: uid & eventtype (both numbers)
+ */
+router.post('/points', PointsPost);
+
+/**
+ * No Parameter
+ */
 router.get('/getQuestionOfDay', QuestionMW);
 
+/**
+ * Body: hafasId (Number)
+ */
 router.post('/getStationMonitor', rnvGetStationMonitorMW);
 
 export default router;
