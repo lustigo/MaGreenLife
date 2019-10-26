6604215a02a2211937aaad21a5faf0fb142b41f2
enrico-kaack <e.kaack@live.de>
2019-10-26 09:48:08 +0200

    Improved: sample data

--- frontend/src/components/Achievement.vue
+++ frontend/src/components/Achievement.vue
@@ -3,17 +3,17 @@
     <v-container>
       <v-row justify="space-around">
         <v-col cols="3">
-          FirstCol
+          <v-img src="https://images.unsplash.com/photo-1569488859134-24b2d490f23f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=640&q=80"></v-img>
         </v-col>
         <v-col cols="8">
           <v-row>
             <v-card-title>
-              Title
+              Kilo Saver
             </v-card-title>
           </v-row>
           <v-row>
             <v-card-text>
-              Description
+              You saved 10 cups and 1KG of CO2!
             </v-card-text>
           </v-row>
         </v-col>
