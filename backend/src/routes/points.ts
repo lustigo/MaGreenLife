b2210c7637f62e14aea88f08c3ec9767d44817ac
enrico-kaack <e.kaack@live.de>
2019-10-26 17:12:00 +0200

    Added: backend friends ranking

--- backend/src/routes/points.ts
+++ backend/src/routes/points.ts
@@ -1,17 +1,17 @@
 async function PointsGet(ctx: any) {
   const score = await ctx.db.get(
-    `select sum(score) count from eventtype, events where eventtype.id = events.type and events.user = ${ctx.request.query.uid}`,
+    `select sum(score) count from eventtype, events where eventtype.id = events.type and events.user = ${ctx.request.query.uid}`
   );
   ctx.body = { score: score.count };
 }
 
 async function PointsPost(ctx: any) {
   const sql =
-    'INSERT INTO events (type, date, user) VALUES (' +
+    "INSERT INTO events (type, date, user) VALUES (" +
     ctx.request.body.eventtype +
     ', strftime("%s", "now"), ' +
     ctx.request.body.uid +
-    ');';
+    ");";
   await ctx.db.exec(sql).catch(console.log);
   ctx.status = 200;
 }
@@ -35,9 +35,27 @@ async function PointsOfFriendsHistory(ctx: any) {
     INNER JOIN eventtype ON eventtype.id = events.type
     INNER JOIN users ON events.user = users.id
     WHERE (events.user IN (select follows from friendlist where friendlist.user = ${ctx.request.query.uid})) OR events.user = ${ctx.request.query.uid}
-    `,
+    `
   );
   ctx.body = result;
 }
 
-export { PointsGet, PointsPost, PointsHistory, PointsOfFriendsHistory };
+async function FriendsRannking(ctx: any) {
+  const result = await ctx.db.all(
+    `select users.name, users.id, sum (score) COUNT from events
+    INNER JOIN eventtype ON eventtype.id = events.type
+    INNER JOIN users ON events.user = users.id
+    WHERE (events.user IN (select follows from friendlist where friendlist.user = ${ctx.request.query.uid})) OR events.user = ${ctx.request.query.uid}
+    GROUP BY users.username
+    `
+  );
+  ctx.body = result;
+}
+
+export {
+  PointsGet,
+  PointsPost,
+  PointsHistory,
+  PointsOfFriendsHistory,
+  FriendsRannking
+};
