95aa5b51b85b0c70793a17da9f9bc7da2aeb3b9e
Lukas Klassen <luftigo@yahoo.de>
2019-10-26 10:33:12 +0200

    feat: Added success Alert when Cupcode was scanned

--- frontend/src/components/ScanCup.vue
+++ frontend/src/components/ScanCup.vue
@@ -1,16 +1,25 @@
 <template>
   <v-container>
-    <qrcode-stream @decode="onDecode"></qrcode-stream>
+    <qrcode-stream @decode="onDecode" v-if="!scanned"></qrcode-stream>
+    <v-alert type="success" v-else>
+      Gut gemacht! Dir wurden 20 Credits gutgeschrieben.
+    </v-alert>
   </v-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      scanned: false
+    };
+  },
   methods: {
-    onDecode: code => {
+    onDecode(code) {
       if (/^mgl-cup/.test(code)) {
         //TODO: Send via sendAction
         console.log(code.slice(8));
+        this.scanned = true;
       }
     }
   }
