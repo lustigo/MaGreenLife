91180fc50870c04ac19cc8b7aebc905b23425639
Lukas Klassen <luftigo@yahoo.de>
2019-10-26 15:34:06 +0200

    feat: Loading the next departures of the station Debug-Output included

--- frontend/src/components/OeffiSelStation.vue
+++ frontend/src/components/OeffiSelStation.vue
@@ -5,11 +5,26 @@
 export default {
   data() {
     return {
-      hafasId: 2417
+	  stations: []
     };
   },
-  created() {
-    this.hafasId = this.$route.params.hafasId;
-  }
+  methods: {
+	  setStations(stations) {
+		  console.log(stations);
+		  this.stations = stations;
+	  }
+  },
+  beforeRouteEnter (to, from, next) {
+	  fetch("http://localhost:4000/getStationMonitor", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          hafasId: to.params.hafasId
+        })
+	  }).then(resp => resp.json().then(s => next(vm => vm.setStations(s)))).catch(console.log);
+}
+
 };
 </script>
