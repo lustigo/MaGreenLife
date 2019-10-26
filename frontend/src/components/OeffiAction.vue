2827b6540211f392062320e46a067e8fb834e246
Lukas Klassen <luftigo@yahoo.de>
2019-10-26 15:13:49 +0200

    feat: Implemented the Oeffi Enter Station Screen

--- frontend/src/components/OeffiAction.vue
+++ frontend/src/components/OeffiAction.vue
@@ -1,7 +1,60 @@
 <template>
-  <v-container> </v-container>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-card-text>
+          <div class="headline mb-1 text--primary">Abfahrtsbahnhof</div>
+          <div>Von welchem Bahnhof fährst du ab?</div>
+        </v-card-text>
+        <v-text-field outlined color="accent" v-model="query"></v-text-field>
+        <v-btn outlined color="accent" block v-on:click="searchStation"
+          >Weiter</v-btn
+        >
+      </v-card>
+      <div style="padding-top: 25px;">
+        <v-card class="mx-auto" max-width="344" outlined>
+          <v-img
+            src="https://cdn.pixabay.com/photo/2018/09/30/19/03/metro-3714290_960_720.jpg"
+            max-width="344"
+          ></v-img>
+        </v-card>
+      </div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      query: ""
+    };
+  },
+  methods: {
+    async searchStation() {
+      if (this.query == "") {
+        return;
+      }
+
+      const resp = await fetch("http://localhost:4000/getStation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          query: this.query
+        })
+      });
+      resp
+        .json()
+        .then(station => {
+          this.$router.push({
+            name: "selStation",
+            params: { hafasId: station.hafasId }
+          });
+        })
+        .catch(console.log);
+    }
+  }
+};
 </script>
---
+++ frontend/src/components/OeffiSelStation.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container> </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      hafasId: 2417
+    };
+  },
+  created() {
+    this.hafasId = this.$route.params.hafasId;
+  }
+};
+</script>
