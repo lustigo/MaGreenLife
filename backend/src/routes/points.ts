67de32d01f6194f44a8aa6cdf3c03eb89ccba254
Lukas Klassen <luftigo@yahoo.de>
2019-10-26 16:11:37 +0200

    fix: Tslint backend

--- backend/src/routes/points.ts
+++ backend/src/routes/points.ts
@@ -1,17 +1,17 @@
 async function PointsGet(ctx: any) {
   const score = await ctx.db.get(
-    `select sum(score) count from eventtype, events where eventtype.id = events.type and events.user = ${ctx.request.query.uid}`
+    `select sum(score) count from eventtype, events where eventtype.id = events.type and events.user = ${ctx.request.query.uid}`,
   );
   ctx.body = { score: score.count };
 }
 
 async function PointsPost(ctx: any) {
   const sql =
-    "INSERT INTO events (type, date, user) VALUES (" +
+    'INSERT INTO events (type, date, user) VALUES (' +
     ctx.request.body.eventtype +
     ', strftime("%s", "now"), ' +
     ctx.request.body.uid +
-    ");";
+    ');';
   await ctx.db.exec(sql).catch(console.log);
   ctx.status = 200;
 }
@@ -22,8 +22,8 @@ async function getPointHistory(uid: number, ctx: any) {
 }
 
 async function PointsHistory(ctx: any) {
-  const sql = `select * from events 
-    INNER JOIN eventtype ON eventtype.id = events.type 
+  const sql = `select * from events
+    INNER JOIN eventtype ON eventtype.id = events.type
     INNER JOIN users ON events.user = users.id
     WHERE  events.user = ${ctx.request.query.uid}`;
   ctx.body = await ctx.db.all(sql);
@@ -31,11 +31,11 @@ async function PointsHistory(ctx: any) {
 
 async function PointsOfFriendsHistory(ctx: any) {
   const result = await ctx.db.all(
-    `select * from events 
-    INNER JOIN eventtype ON eventtype.id = events.type 
+    `select * from events
+    INNER JOIN eventtype ON eventtype.id = events.type
     INNER JOIN users ON events.user = users.id
     WHERE (events.user IN (select follows from friendlist where friendlist.user = ${ctx.request.query.uid})) OR events.user = ${ctx.request.query.uid}
-    `
+    `,
   );
   ctx.body = result;
 }
--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,53 +1,53 @@
-import Router from "koa-router";
-import rnvGetStationMW from "./getStation";
-import rnvGetStationMonitorMW from "./getStationMonitor";
+import Router from 'koa-router';
+import rnvGetStationMW from './getStation';
+import rnvGetStationMonitorMW from './getStationMonitor';
 import {
   PointsGet,
   PointsHistory,
+  PointsOfFriendsHistory,
   PointsPost,
-  PointsOfFriendsHistory
-} from "./points";
-import QuestionMW from "./question";
+} from './points';
+import QuestionMW from './question';
 
 const router = new Router();
 
-router.get("/", ctx => {
-  ctx.body = "Hallo Welt";
+router.get('/', (ctx) => {
+  ctx.body = 'Hallo Welt';
 });
 
 /**
  * Parameter: Uid
  */
-router.get("/points", PointsGet);
+router.get('/points', PointsGet);
 
 /**
  * Parameter: Uid
  */
-router.get("/history", PointsHistory);
+router.get('/history', PointsHistory);
 
 /**
  * Parameter: Uid
  */
-router.get("/friendHistory", PointsOfFriendsHistory);
+router.get('/friendHistory', PointsOfFriendsHistory);
 
 /**
  * Body: uid & eventtype (both numbers)
  */
-router.post("/points", PointsPost);
+router.post('/points', PointsPost);
 
 /**
  * No Parameter
  */
-router.get("/getQuestionOfDay", QuestionMW);
+router.get('/getQuestionOfDay', QuestionMW);
 
 /**
  * Body: hafasId (Number)
  */
-router.post("/getStationMonitor", rnvGetStationMonitorMW);
+router.post('/getStationMonitor', rnvGetStationMonitorMW);
 
 /**
  * Body: query (String that is likely to be the Stationname)
  */
-router.post("/getStation", rnvGetStationMW);
+router.post('/getStation', rnvGetStationMW);
 
 export default router;
