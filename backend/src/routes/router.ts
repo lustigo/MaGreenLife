4f2f851b627533e0daefd45b2ba149ad95a077c6
IndeedNotJames <indeednotjames@mailbox.org>
2019-10-26 18:59:30 +0200

    feat: POST /sendAction

--- backend/src/routes/router.ts
+++ backend/src/routes/router.ts
@@ -1,59 +1,62 @@
-import Router from "koa-router";
-import rnvGetStationMW from "./getStation";
-import rnvGetStationMonitorMW from "./getStationMonitor";
+import Router from 'koa-router';
+import rnvGetStationMW from './getStation';
+import rnvGetStationMonitorMW from './getStationMonitor';
 import {
+  FriendsRannking,
   PointsGet,
   PointsHistory,
   PointsOfFriendsHistory,
   PointsPost,
-  FriendsRannking
-} from "./points";
-import QuestionMW from "./question";
+} from './points';
+import QuestionMW from './question';
+import sendAction from './sendAction';
 
 const router = new Router();
 
-router.get("/", ctx => {
-  ctx.body = "Hallo Welt";
+router.get('/', (ctx) => {
+  ctx.body = 'Hallo Welt';
 });
 
 /**
  * Parameter: Uid
  */
-router.get("/points", PointsGet);
+router.get('/points', PointsGet);
 
 /**
  * Parameter: Uid
  */
-router.get("/history", PointsHistory);
+router.get('/history', PointsHistory);
 
 /**
  * Parameter: Uid
  */
-router.get("/friendHistory", PointsOfFriendsHistory);
+router.get('/friendHistory', PointsOfFriendsHistory);
 
 /**
  * Parameter: Uid
  */
-router.get("/ranking", FriendsRannking);
+router.get('/ranking', FriendsRannking);
 
 /**
  * Body: uid & eventtype (both numbers)
  */
-router.post("/points", PointsPost);
+router.post('/points', PointsPost);
 
 /**
  * No Parameter
  */
-router.get("/getQuestionOfDay", QuestionMW);
+router.get('/getQuestionOfDay', QuestionMW);
 
 /**
  * Body: hafasId (Number)
  */
-router.post("/getStationMonitor", rnvGetStationMonitorMW);
+router.post('/getStationMonitor', rnvGetStationMonitorMW);
 
 /**
  * Body: query (String that is likely to be the Stationname)
  */
-router.post("/getStation", rnvGetStationMW);
+router.post('/getStation', rnvGetStationMW);
+
+router.post('/sendAction', sendAction);
 
 export default router;
